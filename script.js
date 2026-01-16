ec17e42ba5798770aa69a7d43b6b1083dad5c39c
patilprathmesh2606-ctrl <patilprathmesh2606@gmail.com>
2026-01-16 19:12:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // ==============================================
 // Supabase Configuration - UPDATE THESE VALUES!
 // ==============================================
-
 const SUPABASE_URL = 'https://ivvppceuqblhhbqnyfjp.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2dnBwY2V1cWJsaGhicW55ZmpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MTc3ODgsImV4cCI6MjA4Mzk5Mzc4OH0.iM48uGRMQjOVGKqqV7Z3mPGFH4BkWEnZS6T-Zw0dcPs';
 
